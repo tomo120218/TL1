f46716ad6640eb856370d7df862004b7e9b47472
tomohiro byoudou <k024g0017@g.neec.ac.jp>
2026-06-15 12:25:55 +0900

    Update level_editor.py
    TL1_01_06

--- level_editor.py
+++ level_editor.py
@@ -1,46 +1,31 @@
 import bpy
 import math
-
-#ブレンダーに登録するアドオン情報
 bl_info = {
     "name": "レベルエディタ",
-    "author": "Yuki Itai",
-    "version": (1, 0),
-    "blender": (3, 3, 1),
+    "author": "rikuri isobe",
+    "version": (1,0),
+    "blender": (3,3,21),
     "location": "",
     "description": "レベルエディタ",
     "warning": "",
-    #"support": "TESTING",
     "wiki_url": "",
     "tracker_url": "",
-    "category": "Object"
+    "category": "Object",
 }
-
-#メニュー項目描画
-def draw_menu_manual(self, context):
-    #self : 呼び出し元のクラスインスタンス。C++でいうthisポインタ
-    #context : カーソルを合わせた時のホップアップのカスタマイズなどに使用
-
-    #トップバーの「エディターメニュー」に項目（オペレータ）を追加
-    self.layout.operator("wm.url_open_preset", text="Manual", icon="HELP")
-
-
-#オペレータ 頂点を伸ばす
+    
+#オペレーター
 class MYADDON_OT_stretch_vertex(bpy.types.Operator):
     bl_idname = "myaddon.myaddon_ot_stretch_vertex"
     bl_label = "頂点を伸ばす"
     bl_description = "頂点座標を引っ張って伸ばします"
-    #リドゥ、アンドゥ可能オプション
-    bl_options = {'REGISTER', 'UNDO'}
-
-    #メニューを実行したときに呼ばれるコールバック関数
-    def execute(self, context):
-        bpy.data.objects['Cube'].data.vertices[0].co.x += 1.0
-        print("頂点を伸ばしました。")
-
-        #オペレータの終了を通知
-        return {'FINISHED'}
+    bl_options = {'REGISTER','UNDO'}
     
+    def execute(self,context):
+        bpy.data.objects["Cube"].data.vertices[0].co.x += 1.0
+        print("頂点を伸ばしました。")
+        
+        return{'FINISHED'}
+        
 #オペレータICO球生成 
 class MYADDON_OT_create_ico_sphere(bpy.types.Operator):
     bl_idname = "myaddon.myaddon_ot_create_object"
@@ -53,38 +38,45 @@ class MYADDON_OT_create_ico_sphere(bpy.types.Operator):
         print("ICO球を生成しました")
         
         return{'FINISHED'} 
-
-#Add-On有効時コールバック
-def register():
-    # Blenderにクラスを登録
-    for cls in classes:
-        bpy.utils.register_class(cls)
-
-    #メニューに項目を追加
-    bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu.submenu)
-    print("レべルエディタが有効化されました。")
-
-#Add-On無効化時コールバック
-def unregister():
-    #メニューから項目を削除
-    bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
-    # Blenderからクラスを削除
-    for cls in classes:
-        bpy.utils.unregister_class(cls)
-    print("レべルエディタが無効化されました。")
-
+    
+#オペレータシーン出力
+class MYADDON_OT_export_scene(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_export_scene"
+    bl_label = "シーン出力"
+    bl_description = "シーン情報をExportします"
+    
+    def execute(self,context):
+        print("シーン情報をExportします")
+        for object in bpy.context.scene.objects:
+            print(object.type + " - " + object.name)   
+            trans,rot,scale = object.matrix_local.decompose()
+            rot = rot.to_euler()
+            rot.x = math.degrees(rot.x)
+            rot.y = math.degrees(rot.y)
+            rot.z = math.degrees(rot.z)
+            print("Trans(%f,%f,%f)"% (trans.x,trans.y,trans.z))
+            print("Rot(%f,%f,%f)"% (rot.x,rot.y,rot.z))
+            print("Scale(%f,%f,%f)"% (scale.x,scale.y,scale.z))
+            if object.parent:
+                print("Parent:" + object.parent.name)
+            print()
+        
+        print("シーン情報をExportしました")
+        self.report({'INFO'},"シーン情報をExportしました")
+        
+        return{'FINISHED'}
+    
 #トップバーの拡張メニュー
 class TOPBAR_MT_my_menu(bpy.types.Menu):
-    #Blenderがクラスを識別する為の固有の文字列
     bl_idname = "TOPBAR_MT_my_menu"
-    #メニューのラベルとして表示される文字列
     bl_label = "MyMenu"
-    #著作表示用の文字列
     bl_description = "拡張メニュー by " + bl_info["author"]
-
+    
     #サブメニューの描画
-    def draw(self, context):
-
+    def draw(self,context):
+        self.layout.operator("wm.url_open_preset",
+        text="Manual",icon='HELP')
+        
         self.layout.operator(MYADDON_OT_stretch_vertex.bl_idname,
         text=MYADDON_OT_stretch_vertex.bl_label)
         
@@ -94,21 +86,28 @@ class TOPBAR_MT_my_menu(bpy.types.Menu):
         self.layout.operator(MYADDON_OT_export_scene.bl_idname, 
         text=MYADDON_OT_export_scene.bl_label)
         
-    #既存のメニューにサブメニューを追加
-    def submenu(self, context):
-
-        #ID指定でサブメニューを追加
+    def submenu(self,context):
         self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
-
-
-
-
-#Blenderブレンダーに登録するクラスリスト
+    
+#Blanderに登録するクラス
 classes = (
-    MYADDON_OT_stretch_vertex,
-    MYADDON_OT_create_ico_sphere,
-    TOPBAR_MT_my_menu,
+MYADDON_OT_stretch_vertex,
+MYADDON_OT_create_ico_sphere,
+MYADDON_OT_export_scene,
+TOPBAR_MT_my_menu,
 )
-#テスト実行用コード
-if __name__ == "__main__":
-    register()
+    
+def draw_meau_manual(self,context):
+    self.layout.operator("wm.url_open_preset",text="Manual",icon='HELP')
+    
+def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+    bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu.submenu)
+    print("レベルエディタが有効化されました。")
+
+def unregister():
+    bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
+    print("レベルエディタが無効化されました。")
